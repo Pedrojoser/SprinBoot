c3c4bad96dc260b66c0b5838415f7eb477c98a99
Laboratorio De Análisis De Datos Y Simulación Financiero <ladsif@unac.local>
2024-04-16 21:27:56 -0500

    Subida de nuevo archivo

---
+++ crudProgrmacion/src/main/java/com/pedro/crudProgrmacion/CrudProgrmacionApplication.java
@@ -0,0 +1,13 @@
+package com.pedro.crudProgrmacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudProgrmacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudProgrmacionApplication.class, args);
+	}
+
+}
---
+++ crudProgrmacion/src/test/java/com/pedro/crudProgrmacion/CrudProgrmacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedro.crudProgrmacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudProgrmacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
